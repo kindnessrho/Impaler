4f71a3786333aeb4898e98f88cc2c3757a08f655
kindnessrho <ksamuel2.ks.ks@gmail.com>
2025-01-28 11:14:14 -0800

    profile-img,social-links,favicon added

--- assets/js/custom-js.js
+++ assets/js/custom-js.js
@@ -1,88 +1,273 @@
 // floating icons
     
     anime({
-        targets: '#floating-kubernetes',
-        translateY: [
-            { value: -20, duration: 2000, easing: 'easeInOutSine' },
-            { value: 0, duration: 2000, easing: 'easeInOutSine' }
-        ],
+        targets: '#experience-azure',
         rotate: [
-            { value: '15deg', duration: 2500, easing: 'easeInOutSine' }, // Rotate to 15 degrees
-            { value: '0deg', duration: 2500, easing: 'easeInOutSine' }   // Rotate back to original position
+            { value: '10deg', duration: 800, easing: 'easeInOutSine' },  
+            { value: '-10deg', duration: 800, easing: 'easeInOutSine' }, 
+            { value: '0deg', duration: 800, easing: 'easeInOutSine' }    
         ],
-        loop: true,
-        direction: 'alternate'
-    });
+        loop: true 
 
-
-    anime({
-        targets: '#floating-aws',
-        // translateX: [
-        //     { value: 100, duration: 1000, easing: 'easeInOutSine' }, // Move right by 100 pixels
-        //     { value: -50, duration: 1000, easing: 'easeInOutSine' }    // Move back to original position
+        // translateY: [
+        //     { value: -20, duration: 2000, easing: 'easeInOutSine' },
+        //     { value: 0, duration: 2000, easing: 'easeInOutSine' }
         // ],
-        translateY: [
-            { value: -20, duration: 3000, easing: 'easeInOutSine' }, // Move up by 20 pixels
-            { value: 0, duration: 3000, easing: 'easeInOutSine' }    // Move back down to original position
-        ],
-        rotate: [
-            { value: '15deg', duration: 2500, easing: 'easeInOutSine' }, // Rotate to 15 degrees
-            { value: '0deg', duration: 2500, easing: 'easeInOutSine' }   // Rotate back to original position
-        ],
-        loop: true,
-        direction: 'alternate'
-    });
-
-
-    anime({
-        targets: '#floating-azure',
-        translateY: [
-            { value: -20, duration: 3500, easing: 'easeInOutSine' },
-            { value: 0, duration: 3500, easing: 'easeInOutSine' }
-        ],
-        rotate: [
-            { value: '-15deg', duration: 3000, easing: 'easeInOutSine' }, // Rotate to 15 degrees
-            { value: '0deg', duration: 3000, easing: 'easeInOutSine' }   // Rotate back to original position
-        ],
-        loop: true,
-        direction: 'alternate'
-    });
-
-
-    anime({
-        targets: '#floating-docker',
-        // translateX: [
-        //     { value: 100, duration: 1000, easing: 'easeInOutSine' }, // Move right by 100 pixels
-        //     { value: -50, duration: 1000, easing: 'easeInOutSine' }    // Move back to original position
+        // rotate: [
+        //     { value: '15deg', duration: 2500, easing: 'easeInOutSine' }, // Rotate to 15 degrees
+        //     { value: '0deg', duration: 2500, easing: 'easeInOutSine' }   // Rotate back to original position
         // ],
-        translateY: [
-            { value: -20, duration: 3000, easing: 'easeInOutSine' }, // Move up by 20 pixels
-            { value: 0, duration: 3000, easing: 'easeInOutSine' }    // Move back down to original position
-        ],
-        rotate: [
-            { value: '-15deg', duration: 4000, easing: 'easeInOutSine' }, // Rotate to 15 degrees
-            { value: '0deg', duration: 4000, easing: 'easeInOutSine' }   // Rotate back to original position
-        ],
-        loop: true,
-        direction: 'alternate'
+        // loop: true,
+        // direction: 'alternate'
     });
 
 
     anime({
-        targets: '#floating-terraform',
-        // translateX: [
-        //     { value: 100, duration: 1000, easing: 'easeInOutSine' }, // Move right by 100 pixels
-        //     { value: -50, duration: 1000, easing: 'easeInOutSine' }    // Move back to original position
-        // ],
-        translateY: [
-            { value: -20, duration: 4000, easing: 'easeInOutSine' }, // Move up by 20 pixels
-            { value: 0, duration: 4000, easing: 'easeInOutSine' }    // Move back down to original position
-        ],
+        targets: '#experience-kubernetes',
         rotate: [
-            { value: '-15deg', duration: 3500, easing: 'easeInOutSine' }, // Rotate to 15 degrees
-            { value: '0deg', duration: 3500, easing: 'easeInOutSine' }   // Rotate back to original position
+            { value: '10deg', duration: 800, easing: 'easeInOutSine' },  
+            { value: '-10deg', duration: 800, easing: 'easeInOutSine' }, 
+            { value: '0deg', duration: 800, easing: 'easeInOutSine' }    
         ],
-        loop: true,
-        direction: 'alternate'
+        loop: true 
+     
+        // translateY: [
+        //     { value: -20, duration: 3000, easing: 'easeInOutSine' }, // Move up by 20 pixels
+        //     { value: 0, duration: 3000, easing: 'easeInOutSine' }    // Move back down to original position
+        // ],
+        // rotate: [
+        //     { value: '15deg', duration: 2500, easing: 'easeInOutSine' }, // Rotate to 15 degrees
+        //     { value: '0deg', duration: 2500, easing: 'easeInOutSine' }   // Rotate back to original position
+        // ],
+        // loop: true,
+        // direction: 'alternate'
     });
 
+
+    anime({
+        targets: '#experience-docker',
+        rotate: [
+            { value: '10deg', duration: 800, easing: 'easeInOutSine' },  
+            { value: '-10deg', duration: 800, easing: 'easeInOutSine' }, 
+            { value: '0deg', duration: 800, easing: 'easeInOutSine' }    
+        ],
+        loop: true 
+
+        // translateY: [
+        //     { value: -20, duration: 3500, easing: 'easeInOutSine' },
+        //     { value: 0, duration: 3500, easing: 'easeInOutSine' }
+        // ],
+        // rotate: [
+        //     { value: '-15deg', duration: 3000, easing: 'easeInOutSine' }, // Rotate to 15 degrees
+        //     { value: '0deg', duration: 3000, easing: 'easeInOutSine' }   // Rotate back to original position
+        // ],
+        // loop: true,
+        // direction: 'alternate'
+    });
+
+
+    anime({
+        targets: '#experience-terraform',
+        rotate: [
+            { value: '10deg', duration: 800, easing: 'easeInOutSine' },  
+            { value: '-10deg', duration: 800, easing: 'easeInOutSine' }, 
+            { value: '0deg', duration: 800, easing: 'easeInOutSine' }    
+        ],
+        loop: true 
+
+        // translateY: [
+        //     { value: -20, duration: 3000, easing: 'easeInOutSine' }, // Move up by 20 pixels
+        //     { value: 0, duration: 3000, easing: 'easeInOutSine' }    // Move back down to original position
+        // ],
+        // rotate: [
+        //     { value: '-15deg', duration: 4000, easing: 'easeInOutSine' }, // Rotate to 15 degrees
+        //     { value: '0deg', duration: 4000, easing: 'easeInOutSine' }   // Rotate back to original position
+        // ],
+        // loop: true,
+        // direction: 'alternate'
+    });
+
+
+    anime({
+        targets: '#experience-linux',
+        rotate: [
+            { value: '10deg', duration: 800, easing: 'easeInOutSine' },  
+            { value: '-10deg', duration: 800, easing: 'easeInOutSine' }, 
+            { value: '0deg', duration: 800, easing: 'easeInOutSine' }    
+        ],
+        loop: true 
+        
+        // translateY: [
+        //     { value: -20, duration: 4000, easing: 'easeInOutSine' }, // Move up by 20 pixels
+        //     { value: 0, duration: 4000, easing: 'easeInOutSine' }    // Move back down to original position
+        // ],
+        // rotate: [
+        //     { value: '-15deg', duration: 3500, easing: 'easeInOutSine' }, // Rotate to 15 degrees
+        //     { value: '0deg', duration: 3500, easing: 'easeInOutSine' }   // Rotate back to original position
+        // ],
+        // loop: true,
+        // direction: 'alternate'
+    });
+
+
+
+
+    
+    anime({
+        targets: '#experience-ArgoCD',
+        rotate: [
+            { value: '10deg', duration: 800, easing: 'easeInOutSine' },  
+            { value: '-10deg', duration: 800, easing: 'easeInOutSine' }, 
+            { value: '0deg', duration: 800, easing: 'easeInOutSine' }    
+        ],
+        loop: true 
+        
+
+    });
+
+
+    anime({
+        targets: '#experience-Prometheus',
+        rotate: [
+            { value: '10deg', duration: 800, easing: 'easeInOutSine' },  
+            { value: '-10deg', duration: 800, easing: 'easeInOutSine' }, 
+            { value: '0deg', duration: 800, easing: 'easeInOutSine' }    
+        ],
+        loop: true 
+        
+
+    });
+
+
+
+    anime({
+        targets: '#experience-Jenkins',
+        rotate: [
+            { value: '10deg', duration: 800, easing: 'easeInOutSine' },  
+            { value: '-10deg', duration: 800, easing: 'easeInOutSine' }, 
+            { value: '0deg', duration: 800, easing: 'easeInOutSine' }    
+        ],
+        loop: true 
+        
+
+    });
+
+
+
+    anime({
+        targets: '#experience-Grafana',
+        rotate: [
+            { value: '10deg', duration: 800, easing: 'easeInOutSine' },  
+            { value: '-10deg', duration: 800, easing: 'easeInOutSine' }, 
+            { value: '0deg', duration: 800, easing: 'easeInOutSine' }    
+        ],
+        loop: true 
+        
+
+    });
+
+
+
+    anime({
+        targets: '#experience-Helm',
+        rotate: [
+            { value: '10deg', duration: 800, easing: 'easeInOutSine' },  
+            { value: '-10deg', duration: 800, easing: 'easeInOutSine' }, 
+            { value: '0deg', duration: 800, easing: 'easeInOutSine' }    
+        ],
+        loop: true 
+        
+
+    });
+
+
+
+    anime({
+        targets: '#experience-Ansible',
+        rotate: [
+            { value: '10deg', duration: 800, easing: 'easeInOutSine' },  
+            { value: '-10deg', duration: 800, easing: 'easeInOutSine' }, 
+            { value: '0deg', duration: 800, easing: 'easeInOutSine' }    
+        ],
+        loop: true 
+        
+
+    });
+
+
+    anime({
+        targets: '#experience-aws',
+        rotate: [
+            { value: '10deg', duration: 800, easing: 'easeInOutSine' },  
+            { value: '-10deg', duration: 800, easing: 'easeInOutSine' }, 
+            { value: '0deg', duration: 800, easing: 'easeInOutSine' }    
+        ],
+        loop: true 
+        
+
+    });
+
+
+
+
+    function scrollToSection() {
+        document.getElementById('contact').scrollIntoView({ behavior: 'smooth' });
+    }
+
+    function redirectToURL() {
+        window.location.href = "/assets/Robyn Uzor .pdf"; // Replace with your desired URL
+    }
+
+
+    window.onload = function() {
+
+        document.getElementById("form").reset();
+    };
+
+
+    const form = document.getElementById('form');
+const result = document.getElementById('result');
+
+form.addEventListener('submit', function(e) {
+  e.preventDefault();
+  const formData = new FormData(form);
+  const object = Object.fromEntries(formData);
+  const json = JSON.stringify(object);
+  result.innerHTML = "Please wait..."
+
+    fetch('https://api.web3forms.com/submit', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            },
+            body: json
+        })
+        .then(async (response) => {
+            let json = await response.json();
+            if (response.status == 200) {
+                result.innerHTML = "Form submitted successfully!";
+                result.style.border = "2px solid #00CC99";
+                result.style.backgroundColor = "#E6FAF5";
+                result.style.padding = "15px 10px";
+                result.style.borderRadius = "6px";
+            } else {
+                console.log(response);
+                result.innerHTML = json.message;
+            }
+        })
+        .catch(error => {
+            console.log(error);
+            result.innerHTML = "Something went wrong!";
+            result.style.border = "2px solid #EB5757";
+                result.style.backgroundColor = "#FDEEEE";
+                result.style.padding = "15px 10px";
+                result.style.borderRadius = "6px";
+        })
+        .then(function() {
+            form.reset();
+            setTimeout(() => {
+                result.style.display = "none";
+            }, 3000);
+        });
+});
